6881d2f464dd96956a4ff7591a5c7663b02b09d1
csumaoang <csumaoang@academic.rrc.ca>
2024-06-19 23:49:03 -0500

    Added links to navigate to sort by alphabet and missing email

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,50 +1,18 @@
+# app/controllers/customers_controller.rb
+
 class CustomersController < ApplicationController
-    before_action :set_customer, only: [:show, :edit, :update, :destroy]
-  
     def index
       @customers = Customer.all
     end
   
-    def show
+    def alphabetized
+      @customers = Customer.order(:full_name)
+      render :index
     end
   
-    def new
-      @customer = Customer.new
+    def missing_email
+      @customers = Customer.where(email: [nil, ''])
+      render :index
     end
-  
-    def create
-      @customer = Customer.new(customer_params)
-  
-      if @customer.save
-        redirect_to @customer, notice: 'Customer was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @customer.update(customer_params)
-        redirect_to @customer, notice: 'Customer was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @customer.destroy
-      redirect_to customers_url, notice: 'Customer was successfully destroyed.'
-    end
-  
-    private
-      def set_customer
-        @customer = Customer.find(params[:id])
-      end
-  
-      def customer_params
-        params.require(:customer).permit(:full_name, :phone_number, :email, :notes, :image)
-      end
   end
   
